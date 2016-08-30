6d6793cf183166111607019fd3df9c98140e3af7
Gabriel Escobar <gabo9690@gmail.com>
2016-08-30 11:57:45 -0300

    console log de mensajes

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/webhook', function (req, res) {
 app.post('/webhook', function (req, res) {
     console.log(req.body);
     var events = req.body.entry[0].messaging;
-    console.log(req.events);
+    console.log(events);
     for (i = 0; i < events.length; i++) {
         var event = events[i];
         if (event.message && event.message.text) {
