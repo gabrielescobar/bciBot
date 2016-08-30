c0890401f84fb5a6512c51bb594c55815df08449
Gabriel Escobar <gabo9690@gmail.com>
2016-08-30 12:06:51 -0300

    console log de mensajes

--- index.js
+++ index.js
@@ -26,10 +26,10 @@ app.post('/webhook', function (req, res) {
     var events = req.body.entry[0].messaging;
     for (i = 0; i < events.length; i++) {
         var event = events[i];
-        console.log(event.message.text);
-        console.log(event.message);
-        if (event.message && event.message.text) {
 
+        if (event.message && event.message.text) {
+            console.log(event.message.text);
+            console.log(event.message);
             if (!kittenMessage(event.sender.id, event.message.text)) {
                 sendMessage(event.sender.id, {text: "Echos: " + event.message.text});
             }
